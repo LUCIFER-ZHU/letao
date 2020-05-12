7a4904005df44f553ab8dc39370d43e815b73fcc
LUCIFER-ZHU <1147570162@qq.com>
2020-05-12 16:46:14 +0800

    首页完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -8,12 +8,39 @@ NProgress.configure({ showSpinner: false });
 // ajax请求开始时，进度条出现
 // ajax请求完成时，进度条结束
 $(document).ajaxStart(function(){
-  Nprogress.start();
+  NProgress.start();
 })
 
 $(document).ajaxStop(function(){
   setTimeout(() => {
-    Nprogress.done();
+    NProgress.done();
   }, 1000);
   
+})
+
+$(function(){
+  // 二级导航展开合并
+  $('.cate-link').click(function(){
+    $('.second').slideToggle();
+  })
+
+  // 点击切换侧边栏
+  $('.menu').click(function(){
+    $('body').toggleClass('hidemenu')
+  })
+
+  // 退出功能
+  $('.btn-logout').click(function(){
+    $.ajax({
+      url:'/employee/employeeLogout',
+      type:'get',
+      dataType:'json',
+      success:function(info){
+        console.log(info);
+        if(info.success){
+          location.href = './login.html'
+        }
+      }
+    })
+  })
 })
